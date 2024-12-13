acffa0807b69c520adc2d067d47afaa3d865708a
Brian Song <brian.cs.song@gmail.com>
2024-12-13 09:24:45 +0000

    Addition of Users in teh Route

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   #Routes for User resource:
   get("/users", {:controller => "users", :action => "index"})
-  # post("/users/edit", {:controller => "users", :action => "edit"})
-  # get("/users/:username", {:controller => "users", :action => "profile"})
-  # get("/users/:username/liked_photos", {:controller => "users", :action => "liked_photos"})
-  # get("/users/:username/feed",{:controller => "users", :action => "feed"})
-  # get("/users/:username/discover", {:controller => "users", :action => "discover"})
+  post("/users/edit", {:controller => "users", :action => "edit"})
+  get("/users/:username", {:controller => "users", :action => "profile"})
+  get("/users/:username/liked_photos", {:controller => "users", :action => "liked_photos"})
+  get("/users/:username/feed",{:controller => "users", :action => "feed"})
+  get("/users/:username/discover", {:controller => "users", :action => "discover"})
 
   # Routes for the Follow request resource:
     
